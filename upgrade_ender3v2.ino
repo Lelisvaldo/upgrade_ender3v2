0b4ffc97ba096605f27244225d13710b28d025a5
Lelisvaldo <lelisvaldo@outlook.com>
2021-12-04 14:59:30 -0300

    Refactor time out
    Co-authored-by: hmota <heliomotabr@gmail.com>

--- upgrade_ender3v2.ino
+++ upgrade_ender3v2.ino
@@ -6,16 +6,18 @@
 
 #define SCREEN_WIDTH 128    // OLED display width, in pixels
 #define SCREEN_HEIGHT 32    // OLED display height, in pixels
-#define OLED_RESET     -1   // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET    -1    // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+#define TIMEOUT -180
+
 Thermistor temp(A0);
-/*******Communication ports with the Display*********/
-/*    On an arduino UNO:       A4(SDA), A5(SCL)     */
-/*    On an arduino MEGA 2560: 20(SDA), 21(SCL)     */
-/*    On an arduino LEONARDO:   2(SDA),  3(SCL)     */
-/****************************************************/
+/*******Communication ports with the Display**************************************/
+/*    On an arduino UNO:       A4(SDA - CABLE WHITE), A5(SCL - CABLE GREEN)      */
+/*    On an arduino MEGA 2560: 20(SDA - CABLE WHITE), 21(SCL - CABLE GREEN)      */
+/*    On an arduino LEONARDO:   2(SDA - CABLE WHITE),  3(SCL - CABLE GREEN)      */
+/*********************************************************************************/
 int touchPin = 4;
 int ledPins1 = 6;
 int ledPins2 = 7;
@@ -24,7 +26,7 @@ int relauPin = 8;
 bool buttonTouchState = false;
 const int t = 35;
 const int millisecondsToOff = 0;
-float countTimerToOff = -180;
+float countTimerToOff = TIMEOUT;
 
 void setup() {
   Serial.begin(9600);
@@ -53,7 +55,7 @@ void loop() {
   if(getTemperatura() <= t){
     countTimer();
   }else{
-    countTimerToOff = -1.0;
+    countTimerToOff = TIMEOUT;
   }
 
   displayOled();
